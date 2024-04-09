ab0f4e503e56fadc5fd0fcecebaf1409f023264a
mdemong87 <mdemong87@gmail.com>
2024-04-09 19:22:11 +0600

    removed check login middleware

--- app/routes/task/taskRoute.js
+++ app/routes/task/taskRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 // const { getTask, postTask, updateTask, deleteTask, getSingleTask } = require("../../controller/task/taskController");
-const checkLogin = require("../../../middleware/checkLogin");
+// const checkLogin = require("../../../middleware/checkLogin");
 const { getTask, getSingleTask } = require("../../controller/task/taskController");
 
 
 
 //app routes GET request
-router.get('/app/task', checkLogin, getTask);
+router.get('/app/task', getTask);
 
 // //get single task route
 router.get('/app/task/:id', getSingleTask);
